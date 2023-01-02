c7183dfd5dc463c0308c6dbe3b78446ea5aa24dd
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-02 13:09:50 +0530

    New Project

---
+++ src/apps/301560-ChannelWriterComplete/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+
+namespace OrderablePartitionerDemo
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var boundedChannel = Channel.CreateBounded<int>(10);
+
+            _ = Task.Run(async delegate 
+            {
+                for (int i = 0; i < 10 ; i++) 
+                {
+                    await Task.Delay(100);
+                    await boundedChannel.Writer.WriteAsync(i);
+                }
+                boundedChannel.Writer.Complete();
+            });
+
+            // So here, once the Complete() is called, the following loop will exit.
+            //await foreach (var item in boundedChannel.Reader.ReadAllAsync())
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // We can use the above loop or below loop. Any one will work.
+
+            while (await boundedChannel.Reader.WaitToReadAsync())
+            {
+                Console.WriteLine(await boundedChannel.Reader.ReadAsync());
+            }
+
+            // Then the following done is written.
+            Console.WriteLine("Done!!");
+        }
+    }    
+}
---
+++ src/tasks/301560-ChannelWriterComplete/commands.sh
@@ -0,0 +1,9 @@
+cd ../../..
+
+cd src/tasks/301560-ChannelWriterComplete
+
+cd src/apps/301560-ChannelWriterComplete
+
+dotnet run --project ./ChannelWriterComplete.csproj
+
+
