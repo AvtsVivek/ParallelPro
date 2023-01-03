a0b68e26d1689437674bc9193e5ec6e5bbad948d
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-03 12:03:23 +0530

    New Project

---
+++ src/tasks/250270-PassingStateStartMultipleTasksWithWaitAll/commands.sh
@@ -0,0 +1,11 @@
+cd ../../..
+
+cd src/tasks/250270-PassingStateStartMultipleTasksWithWaitAll
+
+cd src/apps/250270-PassingStateStartMultipleTasksWithWaitAll
+
+dotnet run --project ./PassingStateStartMultipleTasksWithWaitAll.csproj -- true
+
+dotnet run --project ./PassingStateStartMultipleTasksWithWaitAll.csproj -- false
+
+
