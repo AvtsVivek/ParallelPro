9243cc365e14dd68e3855dd38e1681395edceff8
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-03 12:49:57 +0530

    New Project

---
+++ src/apps/250275-MultipleTasksWithYield/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.CompilerServices;
+
+class Program
+{
+    private static bool PrintThreadInfo = false;
+
+    private static void Main(string[] args)
+    {
+
+        if (args.Length == 1)
+            bool.TryParse(args[0], out PrintThreadInfo);
+
+        Console.WriteLine($"PrintThreadInfo var is {PrintThreadInfo}");
+
+        if (PrintThreadInfo)
+        {
+            var threadInfo = PrintThreadDetails("From Main Method");
+            Console.WriteLine(threadInfo);
+        }
+
+        string[] messages = { "First task", "Second task", "Third task", "Fourth task" };
+
+        var taskList = CreateTaskList(messages).ToList();
+
+        taskList.ForEach(task => task.Start());
+
+        Task.WaitAll(taskList.ToArray());
+
+        // wait for input before exiting
+        Console.WriteLine("Main method complete. Press enter to finish.");
+        Console.ReadLine();
+    }
+
+    private static IEnumerable<Task> CreateTaskList(string[] messages)
+    {
+        foreach (var message in messages)
+        {
+            yield return new Task(obj => PrintMessage((string)obj!), message);
+        }
+    }
+
+    private static void PrintMessage(object message)
+    {
+        if (PrintThreadInfo)
+        {
+            var threadInfo = PrintThreadDetails($"From PrintMessage Method {message} ");
+            Console.WriteLine(threadInfo);
+        }
+        else
+            Console.WriteLine("Message: {0}", message);
+    }
+
+    private static string PrintThreadDetails(string contextInfo)
+    {
+        var finalString = string.Empty;
+
+        finalString = finalString + Environment.NewLine;
+
+        finalString = $"The context information is: {contextInfo}";
+
+        finalString = finalString + Environment.NewLine + $"The current thread id is {Thread.CurrentThread.ManagedThreadId}.";
+
+        var backgroundString = Thread.CurrentThread.IsBackground ? "a background thread" : "NOT a background thread";
+
+        finalString = finalString + Environment.NewLine + $"This thread is {backgroundString}";
+
+        var threadPoolInfoString = Thread.CurrentThread.IsThreadPoolThread ? "a thread pool thread" : "NOT a thread pool thread";
+
+        finalString = finalString + Environment.NewLine + $"This thread is {threadPoolInfoString}";
+
+        finalString = finalString + Environment.NewLine;
+
+        return finalString;
+    }
+}
---
+++ src/tasks/250275-MultipleTasksWithYield/commands.sh
@@ -0,0 +1,11 @@
+cd ../../..
+
+cd src/tasks/250275-MultipleTasksWithYield
+
+cd src/apps/250275-MultipleTasksWithYield
+
+dotnet run --project ./MultipleTasksWithYield.csproj -- true
+
+dotnet run --project ./MultipleTasksWithYield.csproj -- false
+
+
