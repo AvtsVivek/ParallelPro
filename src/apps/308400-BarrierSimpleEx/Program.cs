1e731c8e6f5dc7e80bee441c0323ac77c95cb9ac
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-13 19:13:52 +0530

    Added more info

--- src/apps/308400-BarrierSimpleEx/Program.cs
+++ src/apps/308400-BarrierSimpleEx/Program.cs
@@ -1,55 +1,97 @@
-﻿// create the array of bank accounts
-
-
-//var dogBreedList = new List<DogBreed>()
-//{
-//    new DogBreed() { BreedName = "German Shepherd" },
-//        new DogBreed() { BreedName = "Labrador" },
-//            new DogBreed() { BreedName = "Golden retriever" },
-//                new DogBreed() { BreedName = "Boxer" },
-//                    new DogBreed() { BreedName = "Tibertan mastiff" },
-//};
-
-// create the total balance counter
-// int totalShoulderHeight = 0;
-
-// define the tasks array
-
-var taskCount = 5;
-
-var taskList = new List<Task>();
-
-// create the barrier
-var barrier = new Barrier(taskCount, (myBarrier) => {
-    Console.WriteLine($"Post-Phase action: phase={myBarrier.CurrentPhaseNumber}");
-});
-
-// loop to create the tasks
-
-for (int i = 0; i < taskCount; i++)
+﻿
+class Program
 {
-    var task = new Task(() => {
-        // signal the barrier
-        Console.WriteLine("First signal...");
-        barrier.SignalAndWait();
-        Console.WriteLine("Second signal...");
-        barrier.SignalAndWait();
-        Console.WriteLine("Third signal...");
-        barrier.SignalAndWait();
-        Console.WriteLine("Fourth signal...");
-        barrier.SignalAndWait();
-        Console.WriteLine("Fifth signal...");
-        barrier.SignalAndWait();
-    });
-    taskList.Add(task);
+    private static void Main(string[] args)
+    {
+
+        Tuple<int, int> commandLineInput = null!;
+
+        if (!TryReadCommandLineArgs(args, out commandLineInput))
+            return;
+
+        var participantCount = commandLineInput.Item1;
+        var taskCount = commandLineInput.Item2;
+
+        var taskList = new List<Task>();
+
+        // create the barrier
+        var barrier = new Barrier(participantCount, (myBarrier) =>
+        {
+            Console.WriteLine($"Post-Phase action: phase={myBarrier.CurrentPhaseNumber}");
+        });
+
+        // loop to create the tasks
+
+        for (int i = 0; i < taskCount; i++)
+        {
+            var task = new Task(() =>
+            {
+                // signal the barrier
+                Console.WriteLine("First signal...");
+                barrier.SignalAndWait();
+                Console.WriteLine("Second signal...");
+                barrier.SignalAndWait();
+                Console.WriteLine("Third signal...");
+                barrier.SignalAndWait();
+                Console.WriteLine("Fourth signal...");
+                barrier.SignalAndWait();
+                Console.WriteLine("Fifth signal...");
+                barrier.SignalAndWait();
+                Console.WriteLine("Sixth signal...");
+                barrier.SignalAndWait();
+                Console.WriteLine("Seventh signal...");
+                barrier.SignalAndWait();
+            });
+            taskList.Add(task);
+        }
+
+        // start the task
+        taskList.ForEach(task => task.Start());
+
+        // wait for all of the tasks to complete
+        Task.WaitAll(taskList.ToArray());
+
+        // Exiting...
+        Console.WriteLine("Exiting...");
+    }
+    private static bool TryReadCommandLineArgs(string[] args, out Tuple<int, int> commandLineInput)
+    {
+        var participantCount = 0;
+        var taskCount = 0;
+        commandLineInput = null!;
+
+        if (args.Length != 2)
+        {
+            Console.WriteLine("There must be 2 int inputs.");
+            return false;
+        }
+
+        if (!int.TryParse(args[0], out participantCount))
+        {
+            Console.WriteLine("The first arg must be an +ve integer greater than 0");
+            return false;
+        }
+
+        if (!int.TryParse(args[1], out taskCount))
+        {
+            Console.WriteLine("The second arg must be an integer");
+            return false;
+        }
+
+        if (participantCount == 0)
+        {
+            Console.WriteLine("The first arg must be an +ve integer greater than 0");
+            return false;
+        }
+
+        if (taskCount == 0)
+        {
+            Console.WriteLine("The second arg must be an +ve integer greater than 0");
+            return false;
+        }
+
+        commandLineInput = new Tuple<int, int>(participantCount, taskCount);
+
+        return true;
+    }
 }
-
-// start the task
-taskList.ForEach(task => task.Start());
-
-// wait for all of the tasks to complete
-Task.WaitAll(taskList.ToArray());
-
-// wait for input before exiting
-Console.WriteLine("Press enter to finish");
-Console.ReadLine();
--- src/tasks/308400-BarrierSimpleEx/commands.sh
+++ src/tasks/308400-BarrierSimpleEx/commands.sh
@@ -4,6 +4,19 @@ cd src/tasks/308400-BarrierSimpleEx
 
 cd src/apps/308400-BarrierSimpleEx
 
-dotnet run --project ./BarrierSimpleEx.csproj
+# The first parameter is participantCount and the second one is taskCount
+# both must be equal in number. 
 
+# This will work.
+dotnet run --project ./BarrierSimpleEx.csproj 3 3
 
+# This will also work.
+dotnet run --project ./BarrierSimpleEx.csproj 4 4
+
+# This does not work. This will be stuck. Four participants are expected and we are creating only 3 tasks.
+# Each participant is a task. 
+dotnet run --project ./BarrierSimpleEx.csproj 4 3
+
+# This will throw exception
+# The number of threads using the barrier exceeded the total number of registered participants
+dotnet run --project ./BarrierSimpleEx.csproj 4 5
