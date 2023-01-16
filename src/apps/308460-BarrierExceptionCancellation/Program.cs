b67d5955bc7d5917d952529e50f114d3994a62a0
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-16 13:44:35 +0530

    Added more info

---
+++ src/apps/308460-BarrierExceptionCancellation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // create a barrier
+        var barrier = new Barrier(2);
+
+        // create a cancellation token source
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        var cancellationToken = cancellationTokenSource.Token;
+
+        // create a task that will complete
+        Task.Factory.StartNew(() => {
+            Console.WriteLine("Good task starting phase 0");
+            barrier.SignalAndWait(cancellationToken);
+            Console.WriteLine("Good task starting phase 1");
+            barrier.SignalAndWait(cancellationToken);
+        }, cancellationToken);
+
+        // create a task that will throw an exception
+        // with a selective continuation that will reduce the 
+        // particpant count in the barrier
+        Task.Factory.StartNew(() => {
+            Console.WriteLine("Bad task 1 throwing exception");
+            throw new Exception();
+        }, cancellationToken).ContinueWith(antecedent => {
+            // reduce the particpant count
+            Console.WriteLine("Cancelling the token");
+            cancellationTokenSource.Cancel();
+        }, TaskContinuationOptions.OnlyOnFaulted);
+
+        // wait for input before exiting
+        Console.WriteLine("Press enter to finish");
+        Console.ReadLine();
+    }
+}
---
+++ src/tasks/308460-BarrierExceptionCancellation/commands.sh
@@ -0,0 +1,8 @@
+cd ../../..
+
+cd src/tasks/308460-BarrierExceptionCancellation
+
+cd src/apps/308460-BarrierExceptionCancellation
+
+dotnet run --project ./BarrierExceptionCancellation.csproj
+
