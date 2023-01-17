b8df1639e7c36586fa85cb9eab71734f5127afb5
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-17 10:59:24 +0530

    New Project

---
+++ src/apps/308830-ManualResetEventCancelToken/Program.cs
@@ -0,0 +1,39 @@
+﻿
+var manualResetEvent = new ManualResetEventSlim();
+
+// create the cancellation token source
+var cancellationTokenSource = new CancellationTokenSource();
+
+var cancellationToken = cancellationTokenSource.Token;
+
+// create and start the task that will wait on the event
+var waitingTask = Task.Factory.StartNew(() => {
+
+    while (true)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Waiting one for the task.");
+        manualResetEvent.Wait(cancellationToken);
+    }
+}, cancellationToken);
+
+Console.WriteLine("Press any key to cancel the manual reset event");
+Console.ReadKey();
+
+manualResetEvent.Set();
+
+
+Console.WriteLine("Press any key to reset the manual reset event");
+Console.ReadKey();
+manualResetEvent.Reset();
+
+Console.WriteLine("Press any key to cancel the manual reset event");
+Console.ReadKey();
+
+cancellationTokenSource.Cancel();
+
+Console.WriteLine("Press any key to exit the program");
+Console.ReadLine();
+Console.WriteLine("Exiting.....");
+
+Console.WriteLine("Bye ...");
---
+++ src/tasks/308830-ManualResetEventCancelToken/commands.sh
@@ -0,0 +1,8 @@
+cd ../../..
+
+cd src/tasks/308830-ManualResetEventCancelToken
+
+cd src/apps/308830-ManualResetEventCancelToken
+
+dotnet run --project ./ManualResetEventCancelToken.csproj
+
