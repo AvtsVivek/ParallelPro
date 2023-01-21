66d036dd6a9a14524d26ccf9c4bdd33a7fe6d56e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-21 18:26:21 +0530

    refactored

--- src/apps/320500-InconsistentCancellation/Program.cs
+++ src/apps/320500-InconsistentCancellation/Program.cs
@@ -5,7 +5,7 @@ var cancellationTokenSource = new CancellationTokenSource();
 var cancellationToken = cancellationTokenSource.Token;
 
 // create the antecedent task
-Task<int> task1 = new Task<int>(() => {
+var task1 = new Task<int>(() => {
     // wait for the token to be cancelled
     cancellationToken.WaitHandle.WaitOne();
     // throw the cancellation exception
@@ -18,20 +18,20 @@ Task<int> task1 = new Task<int>(() => {
 
 // create a continuation
 // *** BAD CODE ***
-Task task2 = task1.ContinueWith((Task<int> antecedent) => {
+var task2 = task1.ContinueWith((Task<int> antecedent) => {
     // read the antecedent result without checking
     // the status of the task
     Console.WriteLine("Antecedent result: {0}", antecedent.Result);
 });
 
 // create a continuation, but use a token
-Task task3 = task1.ContinueWith((Task<int> antecedent) => {
+var task3 = task1.ContinueWith((Task<int> antecedent) => {
     // this task will never be executed 
 }, cancellationToken);
 
 // create a continuation that checks the status
 // of the antecedent
-Task task4 = task1.ContinueWith((Task<int> antecedent) => {
+var task4 = task1.ContinueWith((Task<int> antecedent) => {
     if (antecedent.Status == TaskStatus.Canceled)
     {
         Console.WriteLine("Antecedent cancelled");
