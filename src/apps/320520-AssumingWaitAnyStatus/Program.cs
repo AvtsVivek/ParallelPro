c067ce4ce7ba5f725a18973c9f44939a6d7698e7
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-24 15:25:02 +0530

    Added more info

--- src/apps/320520-AssumingWaitAnyStatus/Program.cs
+++ src/apps/320520-AssumingWaitAnyStatus/Program.cs
@@ -5,32 +5,31 @@ var cancellationTokenSource = new CancellationTokenSource();
 
 var cancellationToken = cancellationTokenSource.Token;
 
-var antecedentTasks = new Task<int>[2];
+var antecedentTasks = new List<Task<int>>();
 
-antecedentTasks[0] = new Task<int>(() => {
+antecedentTasks.Add(new Task<int>(() => {
     while (true)
     {
         // sleep 
         Thread.Sleep(100000);
     }
-});
+}));
 
-antecedentTasks[1] = new Task<int>(() => {
+antecedentTasks.Add(new Task<int>(() => {
     // wait for the token to be cancelled
     cancellationToken.WaitHandle.WaitOne();
     // throw a cancellation exceptioon
     cancellationToken.ThrowIfCancellationRequested();
     // return a result to satisfy the compiler
     return 200;
-}, cancellationToken);
+}, cancellationToken));
 
-Task.Factory.ContinueWhenAny(antecedentTasks, (Task<int> antecedent) => {
+Task.Factory.ContinueWhenAny(antecedentTasks.ToArray(), (Task<int> antecedent) => {
     Console.WriteLine("Result of antecedent is {0}", antecedent.Result);
 });
 
 // start the tasks
-antecedentTasks[1].Start();
-antecedentTasks[0].Start();
+antecedentTasks.ForEach(task => task.Start());
 
 // prompt the user and cancel the token
 Console.WriteLine("Press enter to cancel token");
