9dcd33ada28d9277671a1e68b3648683ad26a1f0
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-24 11:45:05 +0530

    Added more info

--- src/apps/320520-AssumingWaitAnyStatus/Program.cs
+++ src/apps/320520-AssumingWaitAnyStatus/Program.cs
@@ -1,11 +1,13 @@
 ﻿
 
 // create a cancellation token source
-var tokenSource = new CancellationTokenSource();
+var cancellationTokenSource = new CancellationTokenSource();
 
-var tasks = new Task<int>[2];
+var cancellationToken = cancellationTokenSource.Token;
 
-tasks[0] = new Task<int>(() => {
+var antecedentTasks = new Task<int>[2];
+
+antecedentTasks[0] = new Task<int>(() => {
     while (true)
     {
         // sleep 
@@ -13,27 +15,27 @@ tasks[0] = new Task<int>(() => {
     }
 });
 
-tasks[1] = new Task<int>(() => {
+antecedentTasks[1] = new Task<int>(() => {
     // wait for the token to be cancelled
-    tokenSource.Token.WaitHandle.WaitOne();
+    cancellationToken.WaitHandle.WaitOne();
     // throw a cancellation exceptioon
-    tokenSource.Token.ThrowIfCancellationRequested();
+    cancellationToken.ThrowIfCancellationRequested();
     // return a result to satisfy the compiler
     return 200;
-}, tokenSource.Token);
+}, cancellationToken);
 
-Task.Factory.ContinueWhenAny(tasks, (Task<int> antecedent) => {
+Task.Factory.ContinueWhenAny(antecedentTasks, (Task<int> antecedent) => {
     Console.WriteLine("Result of antecedent is {0}", antecedent.Result);
 });
 
 // start the tasks
-tasks[1].Start();
-tasks[0].Start();
+antecedentTasks[1].Start();
+antecedentTasks[0].Start();
 
 // prompt the user and cancel the token
 Console.WriteLine("Press enter to cancel token");
 Console.ReadLine();
-tokenSource.Cancel();
+cancellationTokenSource.Cancel();
 
 // wait for input before exiting
 Console.WriteLine("Press enter to finish");
