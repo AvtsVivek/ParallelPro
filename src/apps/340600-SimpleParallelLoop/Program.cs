3b7f6711f968e56fddac88a108b0be3107482f6c
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-29 16:33:01 +0530

    Added more info

---
+++ src/apps/340600-SimpleParallelLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿class SimpleParallelLoop
+{
+    private static void Main(string[] args)
+    {
+        // create the arrays to hold the data and the results
+        var dataItems = new int[10];
+        var resultItems = new double[10];
+
+        // create the data items
+        for (int i = 0; i < dataItems.Length; i++)
+            dataItems[i] = i;       
+
+        // process the data in a parallel for loop
+        Parallel.For(0, dataItems.Length, (index, loopState) => {
+            resultItems[index] = Math.Pow(dataItems[index], 2);
+        });
+
+        foreach (var item in resultItems)
+            Console.WriteLine(item);       
+
+        // wait for input before exiting
+        Console.WriteLine("Press enter to finish");
+        Console.ReadLine();
+    }
+}
