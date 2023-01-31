c1cc3401d252bf95398772317ab29a4622aab326
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-01-31 10:49:34 +0530

    Added more info

--- src/apps/340700-TaskIdWithInParallelLoop/Program.cs
+++ src/apps/340700-TaskIdWithInParallelLoop/Program.cs
@@ -3,8 +3,12 @@
     private static void Main(string[] args)
     {
         Parallel.For(0, 10, index => {
-            Console.WriteLine("Task ID {0} processing index: {1}",
-                Task.CurrentId, index);
+            Console.WriteLine("Task ID {0} processing index: {1}", Task.CurrentId, index);
+        });
+
+        // Introduces parallelState.
+        Parallel.For(3, 6, (index, parallelState) => {
+            Console.WriteLine("Task ID {0} processing index: {1}, state is {2}", Task.CurrentId, index, parallelState.ToString());
         });
 
         // wait for input before exiting
