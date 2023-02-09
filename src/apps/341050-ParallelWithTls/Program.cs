9d40ac4d11c87a2c316021efeea518d38ff788f8
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-02-09 11:06:48 +0530

    added more info

--- src/apps/341050-ParallelWithTls/Program.cs
+++ src/apps/341050-ParallelWithTls/Program.cs
@@ -8,9 +8,10 @@ class Program
 
 
         var total = 0;
-        Parallel.For(0, 100, index => {
-            Interlocked.Add(ref total, index);
-        });
+        Parallel.For(0, 100, () => 0, (int index, ParallelLoopState loopState, int tlsValue) => {
+            tlsValue += index; 
+            return tlsValue;
+        }, value => Interlocked.Add(ref total, value));
 
         Console.WriteLine($"The total is {total} and the expected is {99*100/2}"); // n(n+1)/2
 
