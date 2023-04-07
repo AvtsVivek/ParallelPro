9d7215105ef4ee492a07dc226e190ee8a9d9fdcb
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-04-07 20:58:32 +0530

    New Project

---
+++ src/apps/360740-PLinqForAll/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Dynamic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // create some source data
+        int[] sourceData = new int[50];
+
+        for (int i = 0; i < sourceData.Length; i++)
+        {
+            sourceData[i] = i;
+        }
+
+        // filter the data and call ForAll()
+        sourceData.AsParallel()
+            .Where(item => item % 2 == 0)
+            .ForAll(item => Console.WriteLine("Item {0} Result {1}", item, Math.Pow(item, 2)));
+
+        // wait for input before exiting
+        Console.WriteLine("Press enter to finish");
+        Console.ReadLine();
+    }
+}
---
+++ src/tasks/360740-PLinqForAll/commands.sh
@@ -0,0 +1,9 @@
+cd ../../..
+
+cd src/tasks/360740-PLinqForAll
+
+cd src/apps/360740-PLinqForAll
+
+dotnet run --project ./PLinqForAll.csproj 
+
+
