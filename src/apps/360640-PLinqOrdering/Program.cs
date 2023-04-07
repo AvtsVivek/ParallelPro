96102a04a6c10630dfc59ecf58b90e7ff4a0244f
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-04-07 19:08:01 +0530

    New Project

--- src/apps/360640-PLinqOrdering/Program.cs
+++ src/apps/360640-PLinqOrdering/Program.cs
@@ -32,62 +32,22 @@ class Program
         Console.WriteLine(args[0]);
 
         // create some source data
-        int[] sourceData = new int[100000];
+        int[] sourceData = new int[10];
+
         for (int i = 0; i < sourceData.Length; i++)
             sourceData[i] = i;
-        
 
-        var myList = new List<KeyValuePair<int, double>>();
+        // preserve order with the AsOrdered() method
+        IEnumerable<double> results =
+            from item in sourceData.AsParallel().AsOrdered()
+            select Math.Pow(item, 2);
 
-        if (args[0] == "AsSequential")
-        {
-            // define a filtering query using keywords
-            //IEnumerable<double> results1
-            //    = from item in sourceData
-            //      where item % 2 == 0
-            //      select Math.Pow(item, 2);
+        // enumerate the results of the parallel query
+        foreach (double d in results)
+            Console.WriteLine("Parallel result: {0}", d);
 
-            // The above and below are same. 
-            // The above is query syntax while the below is fluent syntax. 
-            // I prefer the fluent syntax.
-
-
-            IEnumerable<double> results1
-                = sourceData
-                .Where(item => item % 2 == 0)
-                .Select(item => Math.Pow(item, 2));
-
-            // enumerate the results
-            foreach (var d in results1)
-                Console.WriteLine("Result: {0}", d);
-        }
-
-        if (args[0] == "AsParallel")
-        {
-            //IEnumerable<double> results1
-            //= from item in sourceData.AsParallel()
-            //  where item % 2 == 0
-            //  select Math.Pow(item, 2);
-
-            // The above and below are same. 
-            // The above is query syntax while the below is fluent syntax. 
-            // I prefer the fluent syntax.
-
-            // define a filtering query using extension methods
-            IEnumerable<double> results2
-                = sourceData.AsParallel()
-                .Where(item => item % 2 == 0)
-                .Select(item => Math.Pow(item, 2));
-
-            // enumerate the results
-            foreach (var d in results2)
-                Console.WriteLine("Result: {0}", d);
-
-        }
-
-
-        // wait for input before exiting
-        //Console.WriteLine("Press enter to finish");
-        //Console.ReadLine();
+        // Wait for input before exiting
+        // Console.WriteLine("Press enter to finish");
+        // Console.ReadLine();
     }
 }
---
+++ src/tasks/360640-PLinqOrdering/commands.sh
@@ -0,0 +1,10 @@
+cd ../../..
+
+cd src/tasks/360640-PLinqOrdering
+
+cd src/apps/360640-PLinqOrdering
+
+dotnet run --project ./PLinqOrdering.csproj -- AsSequential
+
+dotnet run --project ./PLinqOrdering.csproj -- AsParallel
+
